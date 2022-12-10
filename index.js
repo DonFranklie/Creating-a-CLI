9e1d651f9def2b497caf25bcc010c01fc9a306b5
Franklin Makokha <107101640+DonFranklie@users.noreply.github.com>
2022-12-10 11:59:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log("Franklin Makokha!");
+
+// Lesson 2
+var readlineSync = require('readline-sync');
+var firstName = readlineSync.question('What is first your name:');
+var lastName = readlineSync.question('What is last your name:');
+var fullName = firstName + lastName
+
+console.log('Oh Hi '+fullName);
+
+// var ageNumber = readlineSync.question('What is your age:', {
+//   hideEchoBack:true //Hides what your input
+// });
+// console.log('Oh Hi '+userName+' so you are '+ageNumber+' years old');
+// console.log("Franklin Welcome to Repl.it");
+
+
